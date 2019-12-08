57ba62be0b4460ca88fd1c7e8d03b24dd3631476
jaronow <waronow123@gmail.com>
2019-12-08 13:54:14 -0700

    added babel to package and configured webpack to use it

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,16 @@ module.exports = {
       {
         test: /\.hbs$/,
         use: ['handlebars-loader']
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
       }
     ]
   }
